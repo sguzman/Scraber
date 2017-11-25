00a53bd491c20fdc55364d1b6733dd67582c73c4
Salvador Guzman <guzmansalv@gmail.com>
2017-11-25 10:04:14 -0800

    moved file to a package

---
+++ src/main/scala/com/github/sguzman/scala/scraber/Main.scala
@@ -0,0 +1,12 @@
+package com.github.sguzman.scala.scraber
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("hello world")
+
+    val url1 = "https://auth.uber.com/login/?next_url=https%3A%2F%2Fpartners.uber.com"
+    val url2 = "https://auth.uber.com/login/handleanswer"
+    val url3 = "https://auth.uber.com/login/handleanswer"
+    val url4 = "https://auth.uber.com/login/handleanswer"
+  }
+}
