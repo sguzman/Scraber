d85e89974c0a26d5433eaceaed183a06c22c2485
Salvador Guzman <guzmansalv@gmail.com>
2017-11-25 11:25:44 -0800

    modularized post request

--- src/main/scala/com/github/sguzman/scala/scraber/Main.scala
+++ src/main/scala/com/github/sguzman/scala/scraber/Main.scala
@@ -1,14 +1,15 @@
 package com.github.sguzman.scala.scraber
 
 import com.github.sguzman.scala.scraber.args.Argv
+import com.github.sguzman.scala.scraber.json.postuser.{UserAnswerMobile, UserHandleAnswerMobile, UserIdentifierMobile, UserMobile}
+import io.circe.generic.auto._
+import io.circe.syntax._
 
-import scalaj.http.Http
+import scalaj.http.{Http, HttpResponse}
 
 object Main {
   def main(args: Array[String]): Unit = {
     val argv = Argv(args)
-
-    println("hello world")
 
     val url1 = "https://auth.uber.com/login/?next_url=https%3A%2F%2Fpartners.uber.com"
     val url2 = "https://auth.uber.com/login/handleanswer"
@@ -18,6 +19,29 @@ object Main {
     val request = Http(url1)
     val response = request.asString
 
-    println(response.body)
+    val body = UserHandleAnswerMobile(
+      UserAnswerMobile("VERIFY_INPUT_MOBILE",
+        UserIdentifierMobile(
+          UserMobile(argv.country, argv.user))),
+      init = true)
+
+    val bodyStr = body.asJson.noSpaces
+
+    val response2 = post(response, bodyStr)
+
+    println(response2.body)
+  }
+
+  def post(lastResponse: HttpResponse[String], body: String) = {
+    val url = "https://auth.uber.com/login/handleanswer"
+
+    val request = Http(url)
+      .postData(body)
+      .header("Cookie", lastResponse.cookies.mkString("; "))
+      .header("Content-Type", "application/json")
+      .header("x-csrf-token", lastResponse.header("x-csrf-token").get)
+    val response = request.asString
+
+    response
   }
 }
