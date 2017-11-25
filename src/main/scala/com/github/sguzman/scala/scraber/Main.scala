3b89561619c5b84079a7bdd29c45f3533382cd56
Salvador Guzman <guzmansalv@gmail.com>
2017-11-25 10:43:34 -0800

    print out get request

--- src/main/scala/com/github/sguzman/scala/scraber/Main.scala
+++ src/main/scala/com/github/sguzman/scala/scraber/Main.scala
@@ -1,12 +1,23 @@
 package com.github.sguzman.scala.scraber
 
+import com.github.sguzman.scala.scraber.args.Argv
+
+import scalaj.http.Http
+
 object Main {
   def main(args: Array[String]): Unit = {
+    val argv = Argv(args)
+
     println("hello world")
 
     val url1 = "https://auth.uber.com/login/?next_url=https%3A%2F%2Fpartners.uber.com"
     val url2 = "https://auth.uber.com/login/handleanswer"
     val url3 = "https://auth.uber.com/login/handleanswer"
     val url4 = "https://auth.uber.com/login/handleanswer"
+
+    val request = Http(url1)
+    val response = request.asString
+
+    println(response.body)
   }
 }
